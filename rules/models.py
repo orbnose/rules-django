76d1e169699cfae51d1bc7208721405ce901ec86
Benjamin Roos <BenRRoos@gmail.com>
2022-11-11 15:25:08 -0600

    condition tests

--- rules/models.py
+++ rules/models.py
@@ -4,26 +4,37 @@ from django.db import models
 
 '''
 Setup: One context_type per variable available to the rules. 
-Property and Operator dictionaries will be laid out like so:
+Property and Operator and Action dictionaries will be laid out like so:
 
-property_dict = {
-    "context_type1" : [func11, func12, ..., func1M],
-    "context_type2" : [func21, func22, ..., func2K],
+property_function_dict = {
+    "context_type1" : [propfunc11, propfunc12, ..., propfunc1M],
+    "context_type2" : [propfunc21, propfunc22, ..., propfunc2K],
     ...
 }
-boolOperator_dict = {
+action_function_dict = {
+   "context_type1" : [actfunc11, actfunc12, ..., actfunc1M],
+   "context_type2" : [actfunc21, actfunc22, ..., actfunc2K],
+    ...
+}
+boolOperator_string_dict = {
     "context_type1" : [jsonlogic_op11, jsonlogic_op21, ..., jsonlogic_op1N],
     "context_type2" : [jsonlogic_op21, jsonlogic_op22, ..., jsonlogc_op2L],
     ...
 }
 
 Properties and operators will be created by running through theses dictionaries and creating new objects for any 
-entries in the list not yet represented by an object.
+entries in the list not yet represented by an object. The property string name can be extracted with funcMK.__name__.
+The operator entries should already be strings.
 
-Each time a rule is to be evaluated, the context dictionary will be laid out like so:
+Each time a rule is to be evaluated, the context and function dictionaries must be laid out like so:
 context_dict = {
     "context_type1" : context_var1,
     "context_type2" : context_var2,
+    ...
+}
+function_dict = {
+    "propfunc11": propfunc11,
+    "actfunc11" : actfunc11,
     ...
 }
 '''
@@ -34,6 +45,58 @@ Create function/script to create new Property/BoolOperator objects and tie a com
 Write function to convert symbolic jsonlogic string into one with complete conditions
 Build the GUI
 '''
+
+class RuleActionPair(models.Model):
+
+    jsonlogic_if_statement = models.JSONField()
+
+class Rule(models.Model):
+
+    name = models.CharField(max_length = 200)
+    logic_string = models.CharField(max_length = 200)
+    jsonlogic_only_boolean_symbols = models.JSONField(blank=True)
+    jsonlogic_full_conditions = models.JSONField(blank=True)
+    num_conditions = models.PositiveIntegerField()
+    rule_action_pair = models.ForeignKey(to=RuleActionPair, on_delete=models.CASCADE)
+
+    def set_jsonlogic_conditions(self):
+
+        for condition in self.condition_set:
+            condition.set_jsonlogic_condition_and_save()
+        
+        self.jsonlogic_full_conditions = self.replace_jsonlogic_symbols_recur( copy.deepcopy(self.jsonlogic_only_boolean_symbols) )
+
+    def replace_jsonlogic_symbols_recur(self, jsonlogic):
+
+        if type(jsonlogic) == dict:
+            # There should only be 1 key per logic string dictionary (one of 'AND', 'OR', 'NOT').
+            key = jsonlogic.keys()[0]
+            value = jsonlogic.values()[0]
+
+            # Convert 'AND' 'OR' 'NOT'
+            if key == 'NOT':
+                key = '!'
+            else:
+                key = key.lower()
+            
+            # Iterate
+            jsonlogic = {}
+            jsonlogic[key] = self.replace_jsonlogic_symbols_recur(value)
+
+        elif type(jsonlogic) == list:
+            for index, symbol in enumerate(jsonlogic):
+                condition_number_match = re.search(r"^@(\d)+$", symbol)
+                if not condition_number_match:
+                    raise ValueError(symbol,' in ',jsonlogic,': improper condition argument')
+                num = condition_number_match(0)
+                jsonlogic[index] = self.condition_set.get(rule_index=num).jsonlogic_condition
+
+        return jsonlogic
+
+class Action(models.Model):
+    function_name = models.CharField(max_length = 200)
+    context_type = models.CharField(max_length = 200)
+    rule_action_pair = models.ForeignKey(to=RuleActionPair, on_delete=models.CASCADE)
 
 class Property(models.Model):
     function_name = models.CharField(max_length = 200)
@@ -55,51 +118,6 @@ class BoolOperator(models.Model):
     jsonlogic_operator = models.CharField(max_length=2, choices=jsonlogic_op_list)
     context_type = models.CharField(max_length = 200)
 
-
-
-class Rule(models.Model):
-
-    name = models.CharField(max_length = 200)
-    logic_string = models.CharField(max_length = 200)
-    jsonlogic_symbols = models.JSONField(blank=True)
-    jsonlogic_conditions = models.JSONField(blank=True)
-    num_conditions = models.PositiveIntegerField()
-
-    def replace_jsonlogic_symbols_iter(self, jsonlogic):
-
-        if type(jsonlogic) == dict:
-            # There should only be 1 key per logic string dictionary (one of 'AND', 'OR', 'NOT').
-            key = jsonlogic.keys()[0]
-            value = jsonlogic.values()[0]
-
-            # Convert 'AND' 'OR' 'NOT'
-            if key == 'NOT':
-                key = '!'
-            else:
-                key = key.lower()
-            
-            # Iterate
-            jsonlogic = {}
-            jsonlogic[key] = self.replace_jsonlogic_symbols_iter(value)
-
-        elif type(jsonlogic) == dict:
-            for index, symbol in enumerate(jsonlogic):
-                condition_number_match = re.search(r"^@(\d)+$", symbol)
-                if not condition_number_match:
-                    raise ValueError(symbol,' in ',jsonlogic,': improper condition argument')
-                num = condition_number_match(0)
-                jsonlogic[index] = self.condition_set.get(rule_index=num).jsonlogic_condition
-
-        return jsonlogic
-
-    def set_jsonlogic_conditions(self):
-
-        for condition in self.condition_set:
-            condition.set_jsonlogic_condition()
-        
-        self.jsonlogic_conditions = self.replace_jsonlogic_symbols_iter( copy.deepcopy(self.jsonlogic_symbols) )
-        self.save()
-
 class Condition(models.Model):
 
     # note: the context type of a condition can be checked via condition.operand_subject.context_type
@@ -113,21 +131,22 @@ class Condition(models.Model):
     operator = models.ForeignKey(to=BoolOperator, on_delete=models.CASCADE)
     jsonlogic_condition = models.JSONField()
 
-    def get_subject(self):
-        if self.freetext_subject:
-            return self.freetext_subject
-        return self.operand_subject.function_name
-    
-    def get_object(self):
-        if self.freetext_object:
-            return self.freetext_object
-        return self.operand_object.function_name
-
     def set_jsonlogic_condition(self):
         if not self.operand_object or not self.operand_object or not self.operator:
             return ValueError('missing condition components')
 
         self.jsonlogic_condition = (
-            { self.operator.jsonlogic_operator : [ { "var": self.get_subject}, { "var": self.get_object } ]}
+            { self.operator.jsonlogic_operator : [self.get_subject_json(), self.get_object_json()] }
         )
-        self.save()
+
+    def get_subject_json(self):
+        return self.get_freetext_or_property_json(self.freetext_subject, self.operand_subject.function_name)
+    
+    def get_object_json(self):
+        return self.get_freetext_or_property_json(self.freetext_object, self.operand_object.function_name)
+
+    def get_freetext_or_property_json(self, freetext_attr, func_name_attr):
+        if freetext_attr:
+            return freetext_attr
+        return { "var" : func_name_attr}
+
--- rules/tests/test_models.py
+++ rules/tests/test_models.py
@@ -1,32 +1,115 @@
 from django.test import TestCase
 
-from ..models import Rule, Condition, Property, BoolOperator
+from ..models import RuleActionPair, Action, Rule, Condition, Property, BoolOperator
 
 class TestCondition(TestCase):
     def setUp(self):
-        self.num_spoons = Property(
+        set_up_properties_and_ops()
+        set_up_floating_conditions()
+
+    def test_number_condition_get_subject_json(self):
+        self.assertEqual(
+            Condition.objects.get(pk=1).get_subject_json(),
+            {"var": "number_of_spoons"},
+        )
+    
+    def test_freetext_condition_get_object_json(self):
+        self.assertEqual(
+            Condition.objects.get(pk=2).get_object_json(),
+            "Big",
+        )
+
+    def test_number_condition_json(self):
+        condition = Condition.objects.get(pk=1)
+        condition.set_jsonlogic_condition()
+        self.assertEqual(
+            condition.jsonlogic_condition,
+            { ">" : [{"var" : "number_of_spoons"}, {"var" : "number_of_forks"}] }
+        )
+    
+    def test_freetext_condition_json(self):
+        condition = Condition.objects.get(pk=2)
+        condition.set_jsonlogic_condition()
+        self.assertEqual(
+            condition.jsonlogic_condition,
+            {"==" : [{"var": "spoon_type"}, "Big"] }
+        )
+
+def set_up_floating_conditions():
+    set_up_conditions( get_shell_rule_with_shell_action_pair(), get_shell_rule_with_shell_action_pair() )
+
+def set_up_conditions(number_rule: Rule, freetext_rule: Rule):
+#dependent on set_up_properties_and_ops()
+
+    _ = Condition( # pk = 1
+            operand_subject = Property.objects.get(function_name = "number_of_spoons"),
+            operand_object = Property.objects.get(function_name = "number_of_forks"),
+            operator = BoolOperator.objects.get(jsonlogic_operator = ">"),
+            jsonlogic_condition = {},
+            rule_index = 1,
+            rule = number_rule
+        )
+    _.save()
+    _ = Condition( # pk = 2
+            operand_subject = Property.objects.get(function_name = "spoon_type"),
+            operand_object = Property.objects.get(function_name = "spoon free text"),
+            freetext_object = "Big",
+            operator = BoolOperator.objects.get(jsonlogic_operator = "=="),
+            jsonlogic_condition = {},
+            rule_index = 1,
+            rule = freetext_rule
+        )
+    _.save()
+
+def get_shell_rule_with_shell_action_pair():
+    rap = RuleActionPair(
+        jsonlogic_if_statement = {},
+    )
+    rap.save()
+    _ = Rule( # pk = 1
+        name = "shell",
+        logic_string = "",
+        jsonlogic_only_boolean_symbols = {},
+        jsonlogic_full_conditions = {},
+        num_conditions = 1,
+        rule_action_pair = rap,
+    )
+    _.save()
+    return _
+
+def set_up_properties_and_ops():
+    _ = Property(
             function_name = "number_of_spoons",
             is_free_text = False,
             context_type = "number",
         )
-        self.num_forks = Property(
+    _.save()
+    _ = Property(
             function_name = "number_of_forks",
             is_free_text = False,
             context_type = "number",
         )
-        self.gt_op = BoolOperator(
+    _.save()
+    _ = Property(
+            function_name = "spoon_type",
+            is_free_text = False,
+            context_type = "spoon_string"
+        )
+    _.save()
+    _ = Property(
+            function_name = "spoon free text",
+            is_free_text = True,
+            context_type = "spoon_string"
+        )
+    _.save()
+    _ = BoolOperator(
             jsonlogic_operator = '>',
             context_type = "number",
         )
-        self.simple_rule = Rule(
-            name = "Simple Test Rule",
-            logic_string = "@1",
-            num_conditions = 1,
+    _.save()
+    _ = BoolOperator(
+            jsonlogic_operator = '==',
+            context_type = "number"
         )
-        self.simple_condition = Condition(
-            rule = self.simple_rule,
-            rule_index = 1,
-            operand_subject = self.num_spoons,
-            operand_object = self.num_forks,
-            operator = self.gt_op,
-        )
+    _.save()
+
